2b5c723ee7e6ddb58126e2ae5e48ed6ea402d9c0
sdshuvo98 <hellosdshuvo@gamil.com>
2022-05-19 03:12:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#3de57b",
+          secondary: "#8ce9ea",
+          accent: "#37ccab",
+          neutral: "#171C21",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
